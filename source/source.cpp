a209f4acbd61ec60030794e34f5e1b2d744aaae6
a-mikhaylov <lexarobotr2d2@gmail.com>
2021-12-02 09:29:16 +0300

    add template for pipe in source

--- source/source.cpp
+++ source/source.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello world" << std::endl;
+    //double timeres;
+	int iter = 0;
+	int min, max, n;
+	
+	cout << "Input count of elements: ";
+	cin >> n;
+	cout << "Inpit min value: ";
+	cin >> min;
+	cout << "Input max value: ";
+	cin >> max;
+
+	int* arr = new int[n];
+
+	cout << n << " ";
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % (max - min + 1) + min;
+		cout << arr[i] << " ";
+
+		iter++;
+	}
+
+	//timeres = (double)clock() / 1000;
+	//cout << iter << " " << timeres << " ";
+	cout << "DONE! Every items writen" << endl;
 }
